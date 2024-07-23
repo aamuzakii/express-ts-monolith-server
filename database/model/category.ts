38c25e516982e36653e4b2a9ccae54ea09d9ad07
Muzaki新联想 <aamuzakii@gmail.com>
2024-07-23 08:57:00 +0700

    add category & user model

--- database/model/category.ts
+++ database/model/category.ts
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   image_url: { type: String, required: true },
+  parent_category_id: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product", schema);
